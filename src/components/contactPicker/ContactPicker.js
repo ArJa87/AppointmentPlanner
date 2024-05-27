fa6d176114a08415c3784f6cba466d7f2b2f71d7
J.Herrera <jaarheom@gmail.com>
2024-05-27 19:56:14 -0400

    basic functionality added

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ( {
+  contacts,
+  onChange,
+  value,
+  name
+}) => {
   return (
-    <></>
+    <select aria-label="Contact Picker" onChange={onChange} value={value} name={name}>
+      <option value={""} key={-1}>No Contact Selected</option>
+      {contacts.map((contact) => {
+       return ( 
+        <option key={contact} value={contact}>{contact}</option>
+      );
+      })}
+    </select>
   );
 };
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,19 +3,42 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
-
+export const ContactsPage = ( { contacts, addContact}) => {
+  
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [duplicate, setDuplicate] = useState(false);
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
-  };
+   
+    if (duplicate === false) {
+      addContact(name, phone, email)
+      setName("");
+      setPhone("");
+      setEmail("");
+      
+      //const form = document.getElementById('contactForm');
+      //const submitter = document.querySelector('button[value=submit]');
+      //const formData = new FormData(form, submitter);
+      //addContact(formData.name, formData.phone , formData.email);
+      
+    }
+  }; 
+  
+  useEffect(() => {
+    for (let i = 0; i < contacts.length; i++) {
+      if (name === contacts[i].name) {
+        setDuplicate(true);
+      } else {
+        setDuplicate(false);
+      }
+  }},[name, contacts, duplicate]);
+
+  
+    
+  
 
   /*
   Using hooks, check for contact name in the 
@@ -25,11 +48,17 @@ export const ContactsPage = () => {
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact {duplicate ? " - Name Already Exists" : ""}</h2>
+        <ContactForm 
+        name={name} setName={setName}
+        phone={phone} setPhone={setPhone}
+        email={email} setEmail={setEmail}
+        handleSubmit={handleSubmit} /> 
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList tiles={contacts} />
       </section>
     </div>
   );
